859fcb26e711e4e9f2e91713e5aa4ed1038a5bb7
gucci <ahmet.altay61@yandex.com>
2019-12-03 14:49:19 +0300

    Sipariş ekle butonu eklenip fonksiyon değeri atandı.

--- mobil/restappi/lib/TablePage.dart
+++ mobil/restappi/lib/TablePage.dart
@@ -58,8 +58,10 @@ class tablePageState extends State {
                 return Stack(
                   children: <Widget>[
                     GestureDetector(
-                      onTap:() {siparisiGoster(context,cekilenMasalar[index],cekilenMasalar[index].siparis.length-1);},
-
+                      onTap: () {
+                        siparisiGoster(context, cekilenMasalar[index],
+                            cekilenMasalar[index].siparis.length - 1);
+                      },
                       child: Container(
                         padding: EdgeInsets.all(10.0),
                         margin: EdgeInsets.all(10.0),
@@ -87,6 +89,9 @@ class tablePageState extends State {
                     Align(
                       alignment: Alignment.bottomLeft,
                       child: GestureDetector(
+                        onTap: (){
+                          siparisEkle(context,cekilenMasalar,index);
+                        },
                         child: Stack(
                           children: <Widget>[
                             CircleAvatar(
@@ -118,7 +123,6 @@ class tablePageState extends State {
     var gelenJson = await DefaultAssetBundle.of(context)
         .loadString("assets/localDeneme.json");
 
-
     List<Masa> masalar = (json.decode(gelenJson) as List)
         .map((mapYapisi) => Masa.fromJson(mapYapisi))
         .toList();
@@ -137,41 +141,65 @@ class tablePageState extends State {
     return renkler;
   }
 
-  siparisiGoster(BuildContext ctx, Masa cekilenMasalar,int index ){
+  siparisiGoster(BuildContext ctx, Masa cekilenMasalar, int index) {
     debugPrint(cekilenMasalar.siparis[index].urunadi);
-    showDialog(context: ctx,
-      builder: (ctx){
-      return AlertDialog(
-        title: Text("Siparişler",style:TextStyle(fontSize: 40,color: Colors.white),),
-        content: Text(siparisilistele(cekilenMasalar,index),style: TextStyle(fontSize: 30,color: Colors.black,fontWeight:FontWeight.bold),),
-        actions: <Widget>[
-          FlatButton(
-            onPressed: (){
-              Navigator.of(ctx).pop();
-            }
-          )
-        ],
-        backgroundColor: Color.fromARGB(255, 68, 85, 99),
-      );
-      }
-
-
-
-    );
+    showDialog(
+        context: ctx,
+        builder: (ctx) {
+          return AlertDialog(
+            title: Text(
+              "Siparişler",
+              style: TextStyle(fontSize: 40, color: Colors.white),
+            ),
+            content: Text(
+              siparisilistele(cekilenMasalar, index),
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
+            ),
+            actions: <Widget>[
+              FlatButton(onPressed: () {
+                Navigator.of(ctx).pop();
+              })
+            ],
+            backgroundColor: Color.fromARGB(255, 68, 85, 99),
+          );
+        });
   }
 
   String siparisilistele(Masa cekilenMasalar, int index) {
     String siparis = "";
-    for(int i=0;i<index+1;i++){
-
-      siparis+=cekilenMasalar.siparis[i].adedi.toString()+" "+cekilenMasalar.siparis[i].urunadi.toString()+'\n';
-
-
+    if(cekilenMasalar.masadurumu){
+      for (int i = 0; i < index + 1; i++) {
+        siparis += cekilenMasalar.siparis[i].adedi.toString() +
+            " " +
+            cekilenMasalar.siparis[i].urunadi.toString() +
+            '\n';
+      }
     }
     return siparis;
+  }
+
+  void siparisEkle(BuildContext context, List<Masa> cekilenMasalar, int index) {
+    showDialog(
+        context: context,
+        builder: (ctx) {
+          return AlertDialog(
+            title: Text(
+              "Sipariş ekle",
+              style: TextStyle(fontSize: 40, color: Colors.white),
+            ),
+            content: Text("aa"),
+            actions: <Widget>[
+              FlatButton(onPressed: () {
+                Navigator.of(ctx).pop();
+              })
+            ],
+            backgroundColor: Color.fromARGB(255, 68, 85, 99),
+          );
+        });
 
 
   }
 }
-
-
